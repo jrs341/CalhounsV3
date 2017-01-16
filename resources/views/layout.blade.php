dc5828bd176d874f315abb5e6d74c48f64dc1d3c
Jason Sinn <jrs341@me.com>
2017-01-15 23:28:29 -0600

    added content to layout and welcome blades

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,23 +1,128 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
     <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="RV Park">
+    <meta name="author" content="Jason Sinn">
+    <link rel="shortcut icon" href="../public/css/images/favicon.ico" />
 
-        <title>@yield('title')</title>
+    <title>@yield('title')</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Bootstrap Core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom CSS -->
+    <link href="style.php" rel="stylesheet" type="text/css">
+    <!-- @yeild(css) -->
+</head>
 
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-      <div class="container">
-        <h1> This is the layout</h1>
-        @yield('content')
-      </div>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div id='logo'>
+            <a href="index.html">
+                <img id="logo" src="./images/circleC.png">
+            </a>
+            </div>
 
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               
+                <a class="navbar-brand" href="index.html">Calhoun's Riverside RV Retreat</a>
+
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.html">About</a>
+                    </li>
+                    
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                   
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Additional Information<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="rates.html">Current Rates</a>
+                            </li>
+                            <li>
+                                <a href="cabins.html">Cabins</a>
+                            </li>
+                            <li>
+                                <a href="rules.html">Rules</a>
+                            </li>
+                            <li>
+                                <a href="rentalAgreements.html">Rental Agreements</a>
+                            </li>
+                            <li>
+                                <a href="boatramp.html">Boat Ramp</a>
+                            </li>
+                            <li>
+                                <a href="meetCalhoun.html">Meet Calhoun</a>
+                            </li>
+                            <li>
+                                <a href="http://www.lacrossealertsmobile.com/v1.2/#device-7FFF4349485CC4E6">Current Weather</a>
+                            </li>
+                            <li>
+                                <a href="http://www.accuweather.com/en/us/port-lavaca-tx/77979/weather-forecast/335931">Local Weather Radar</a>
+                            </li>
+                            <li>
+                                <a href="riverLevel.html">River Level Info</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    
+    <div class="container">
+    @yield('content')
+    </div>
+
+    <footer>
+    <div class="row">
+        <div class="social col-sm-4 col-sm-offset-4">         
+            <a href="https://twitter.com/CalhounsRV?lang=en" class="icon-button twitter"><i class="fa fa-twitter"></i><span></span></a>
+            <a href="http://facebook.com" class="icon-button facebook"><i class="fa fa-facebook"></i><span></span></a>
+            <a href="http://plus.google.com" class="icon-button google-plus"><i class="fa fa-google-plus"></i><span></span></a> 
+        </div>
+    </div>
+    <div class="row">
+        <div class="social col-lg-12">
+            </br>
+            </br>
+                <p>Copyright &copy; Calhoun's Riverside RV Retreat 2016</p>
+        </div>
+    </div>
+    </footer>
+
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
+      </script>
+
+      <script>
+        $('.carousel').carousel({
+        interval: 5000 //changes the speed
+        })
+      </script>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,72 @@
 @extends('layout')
 
 @section('title')
-Welcome
+Calhoun's Riverside RV Retreat
+@stop
+
+@section('css')
+<link href="css/style.css" rel="stylesheet">
 @stop
 
 @section('content')
-  <h1>Welcome Page</h1>
+<header id="header" class="headerImage">
+        <!-- <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+        <span class="sr-only">Loading...</span> -->
+</header>
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">
+            Your Front Row Seat to a Texas Gem, The Guadalupe River
+        </h1>
+        <p>
+            Calhoun’s Riverside RV Retreat offers visitors the best of the Guadulupe River. Just outside Tivoli, Texas, and a short drive from Victoria, Port O'Connor and Point Comfort, Calhoun’s is centrally located and filled 
+            with wildlife. Once you see it you’ll know: Calhoun’s is unlike any other Texas RV park. Here, visitors can take advantage of some of the best fishing, bow fishing, boating, and canoing and kayaking in Texas, or simply enjoy the river from their RV! Just 17 miles from Port Lavaca, 30 miles from Rockport and Victoria, Calhoun’s offers a peaceful RV camping experience nestled beneath giant oak trees
+
+			Forget trying to get a spot on the bay, Calhoun’s is just 5 short miles by boat from the San Antonio Bay. The river is plenty deep enough to accommodate larger boats, and with Calhoun’s private boat ramp, getting there is easy. Located in the center of the Crescent Coast, visitors can boat their way to the Gulf of Mexico, while enjoying an abundancy of wildlife.
+
+			Calhoun’s offers more than a little something for everyone. Whether you enjoy deer, water fowel or feral hog hunting, this is a sportsmen’s paradise. If you’re a Winter Texan looking for an alternative to the crowds, Calhoun’s is a unique Texas RV Park centrally located to the region’s historic and resort towns. Tired of RV parks without a tree in sight? Wanting a little room to get comfortable? Seeking a little adventure on the river? Calhoun’s is a different kind of place. Paddle down the river one day, take a drive to Corpus Christi the next and enjoy all the variety in this quiet corner of the Crescent Coast. 
+
+			Owned and operated by a U.S. Army veteran, Calhoun’s is all about providing sportsmen, Winter Texans and families with a great place to experience the best of Texas!
+
+			Need lodging? We have cabins!
+        </p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+    	<div class="panel panel-default">
+        	<div class="panel-heading">
+            	<h4><i class="fa fa-fw fa-check"></i> Contact </h4>
+        	</div>
+        	<div class="panel-body">
+            	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+            	<a href="#" class="btn btn-default">Learn More</a>
+        	</div>
+    	</div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4><i class="fa fa-fw fa-gift"></i>Amenities</h4>
+            </div>
+            <div class="panel-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                <a href="#" class="btn btn-default">Learn More</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4><i class="fa fa-fw fa-compass"></i>Park Map</h4>
+            </div>
+            <div class="panel-body">
+            	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+            <a href="#" class="btn btn-default">Learn More</a>
+            </div>
+        </div>
+    </div>
+</div>
+        
 
 @stop
